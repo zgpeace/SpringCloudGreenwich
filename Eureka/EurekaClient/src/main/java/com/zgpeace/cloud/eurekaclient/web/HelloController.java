ec62c0ef69b8b10e732201fe02956ddd3612c5fa
zgpeace <zgpeace@gmail.com>
2020-02-10 08:27:52 +0800

    Eureka

---
+++ Eureka/EurekaClient/src/main/java/com/zgpeace/cloud/eurekaclient/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.zgpeace.cloud.eurekaclient.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+}
